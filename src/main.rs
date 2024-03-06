186591c7274adf7c73ba34ecafe7e1dcb854d516
frosty <inthishouseofcards@gmail.com>
2024-03-06 15:19:54 -0500

    less polite (no please)

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,20 @@ mod interaction;
 mod models;
 
 fn main() -> eyre::Result<()> {
-    let video_mode_selection = select_from_enum::<VideoMode>("Please choose a video mode")?;
+    let video_mode_selection = select_from_enum::<VideoMode>("Select a video mode")?;
     let selected_video_mode = VideoMode::iter().nth(video_mode_selection).unwrap();
 
-    let auto_boot_selection = select_from_enum::<AutoBoot>("Please choose an autoboot option")?;
+    let auto_boot_selection = select_from_enum::<AutoBoot>("Select an autoboot option")?;
     let selected_auto_boot = AutoBoot::iter().nth(auto_boot_selection).unwrap();
 
     let auto_boot_delay = if selected_auto_boot != AutoBoot::NONE {
-        Some(input_with_range(
-            "Please enter an autoboot delay",
-            5,
-            0..=10,
-        )?)
+        Some(input_with_range("Enter an autoboot delay", 5, 0..=10)?)
     } else {
         None
     };
 
     let sd_card_path = string_input(
-        "Please enter the path of your SD card (leave empty for current directory)",
+        "Enter the path of your SD card (leave empty for current directory)",
         String::from(""),
     )?;
 
